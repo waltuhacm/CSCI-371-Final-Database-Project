d42d62ff309877fe0b4a8bf44b7bf9c1eed7cbbe
Sean Rotich <seanrotich@gmail.com>
2025-04-07 10:22:29 -0500

    Starting Point w/ Connection to Database

---
+++ connection.php
@@ -0,0 +1,21 @@
+<?php
+/* Database credentials. Assuming you are running MySQL server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'rei.cs.ndsu.nodak.edu');
+define('DB_USERNAME', 'jayden_mills_371s25');
+define('DB_PASSWORD', 'DRLS0rvQSN0!');
+define('DB_NAME', 'jayden_mills_db371s25');
+
+/* Attempt to connect to MySQL database */
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if ($conn === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+} else {
+
+    echo("db is done");
+}
+?>
+
+
+
